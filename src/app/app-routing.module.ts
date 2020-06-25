3788e4bf86aad94dba1c0a58adad7e43afe5afe7
VincentDias <vincentdias.pro@gmail.com>
2020-06-25 18:44:31 +0200

    sms service works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { LoginComponent } from './pages/login/login.component';
 import { SignUpComponent } from './pages/login/sign-up/sign-up.component';
 import { PatientListComponent } from './pages/home/patient-list/patient-list.component';
 import { ChatComponent } from './pages/home/patient-list/chat/chat.component';
-import { PatientDetailsComponent } from "./pages/home/patient-list/patient-details/patient-details.component";
+import { PatientDetailsComponent } from './pages/home/patient-list/patient-details/patient-details.component';
 
 
 const routes: Routes = [
@@ -12,7 +12,7 @@ const routes: Routes = [
   { path: 'sign-up', component: SignUpComponent },
   { path: 'home', component: PatientListComponent },
   { path: 'chat', component: ChatComponent },
-  { path: 'patient-details', component: PatientDetailsComponent }
+  { path: 'details', component: PatientDetailsComponent }
 
 
 ];
