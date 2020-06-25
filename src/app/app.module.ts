83f949a2725d5c9666c1d7260928f082d9f7d56d
Lexhoo <lexhoo@yahoo.fr>
2020-06-25 22:59:47 +0200

    login ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,21 @@ import { AppComponent } from './app.component';
 import { TableComponent } from './pages/home/patient-list/table/table.component';
 import { LoginComponent } from './pages/login/login.component';
 import { FormsModule } from '@angular/forms';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { SignUpComponent } from './pages/login/sign-up/sign-up.component';
 import { PatientListComponent } from './pages/home/patient-list/patient-list.component';
 import { ChatComponent } from './pages/home/patient-list/chat/chat.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { PatientDetailsComponent } from './pages/home/patient-list/patient-details/patient-details.component';
 import { ChatHistoricComponent } from './pages/home/patient-list/patient-details/chat-historic/chat-historic.component';
 import { SmsEditorComponent } from './pages/home/patient-list/patient-details/sms-editor/sms-editor.component';
+import { RegisterUserComponent } from '../app/register-user/register-user.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatIconModule} from '@angular/material/icon';
 
 
 @NgModule({
@@ -30,7 +35,8 @@ import { SmsEditorComponent } from './pages/home/patient-list/patient-details/sm
     ChatComponent,
     PatientDetailsComponent,
     ChatHistoricComponent,
-    SmsEditorComponent
+    SmsEditorComponent,
+    RegisterUserComponent
   ],
   imports: [
     BrowserModule,
@@ -39,10 +45,15 @@ import { SmsEditorComponent } from './pages/home/patient-list/patient-details/sm
     BrowserAnimationsModule,
     MatTableModule,
     MatToolbarModule,
+    MatCardModule,
     FormsModule,
     MatButtonModule,
     MatInputModule,
     MatFormFieldModule,
+    MatTabsModule,
+    MatCheckboxModule,
+    MatIconModule
+
   ],
   providers: [],
   schemas: [
