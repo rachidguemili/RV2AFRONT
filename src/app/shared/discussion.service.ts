58f7ea29e2bf9c9af12b1c67a5e499cc3dbb74aa
Lexhoo <lexhoo@yahoo.fr>
2020-06-26 06:24:34 +0200

    style loggin

---
+++ src/app/shared/discussion.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ChatService } from './chat.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DiscussionService {
+  private url = 'http://localhost:8082/';
+
+  constructor(private http: HttpClient ,private chat : ChatService) { }
+
+
+
+  putDiscussion(message : []) {
+    return this.http.put(this.url + 'chat', this.chat.discussion);
+  }
+}
