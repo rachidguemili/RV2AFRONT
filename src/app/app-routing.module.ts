a96cde840b801f9a0460795e8579146cd8b4d23d
Lexhoo <lexhoo@yahoo.fr>
2020-06-26 07:23:52 +0200

    update style 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
 import { SignUpComponent } from './pages/login/sign-up/sign-up.component';
 import { PatientListComponent } from './pages/home/patient-list/patient-list.component';
 import { ChatComponent } from './pages/home/patient-list/chat/chat.component';
@@ -8,7 +7,7 @@ import { PatientDetailsComponent } from './pages/home/patient-list/patient-detai
 
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
+  { path: '', component: ChatComponent },
   { path: 'sign-up', component: SignUpComponent },
   { path: 'home', component: PatientListComponent },
   { path: 'chat', component: ChatComponent },
