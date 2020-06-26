80f9492d36896ece5aaba68674c7718e257992bf
Rachid Guemili <rachidwilder33@gmail.com>
2020-06-26 14:22:57 +0200

    fixe url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,9 +11,9 @@ import { TableComponent } from './pages/home/patient-list/table/table.component'
 
 const routes: Routes = [
   { path: '', component: RegisterComponent },
-  { path: 'home', component: TableComponent},
-  { path: 'details', component: PatientDetailsComponent },
-  { path: 'chat', component: ChatComponent },
+  { path: 'home', component: TableComponent },
+  { path: 'detail/:id', component: PatientDetailsComponent },
+  { path: 'chat/:id', component: ChatComponent },
 
 
 
